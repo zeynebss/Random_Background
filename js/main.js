553c93b53e57671392eb3d3edaa6fb8458331b69
zeynebss <zezh561@gmail.com>
2022-01-05 16:18:50 +0400

    Click and Change

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var btn=document.getElementById("btn-click").onclick=function(){
+    document.body.style.backgroundColor='rgb(' + Math.round(Math.random()*255) +
+    ','+ Math.round(Math.random()*255) + ','
+    +Math.round(Math.random()*255) +')'
+}
